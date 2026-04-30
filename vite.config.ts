5bfd3dc8b05d7b4c64a7d7fe7004c8b41e00485a
Sreyanshu Epari <apple@Mac.mshome.net>
2026-04-30 10:30:52 +0530

    Fix typescript errors for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  return {
-    plugins: [react()],
-    base: command === "build" ? "/Portfolio_Sreyanshu_Epari/" : "/",
-  };
+export default defineConfig({
+  plugins: [react()],
 });
